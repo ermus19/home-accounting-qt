fbbdba1a042fdfb012bc0c5ef06cd5891766a5e5
ermus19 <ermus19@hotmail.com>
2017-06-22 00:47:20 +0200

    Added minimun size to the mainWindow

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QCoreApplication::setApplicationName("home-accounting-qt");
+
     mainWindow mainWin;
     mainWin.setMinimumSize(700, 500);
     mainWin.show();
