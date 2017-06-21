549a10c8fc8c5c7cc20db62cd528d76fd41a159a
ermus19 <ermus19@hotmail.com>
2017-06-22 00:48:11 +0200

    Added DB initialization to the mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "dbconnection.h"
+
+const QString DRIVER("QSQLITE");
 
 mainWindow::mainWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::mainWindow)
 {
     ui->setupUi(this);
+
+    //Check if sqlite driver is available
+    if (!QSqlDatabase::drivers().contains(DRIVER))
+        QMessageBox::critical(this, "Error loading database", "SQLITE driver not found");
+
+    // initialize the database
+    QSqlError dbError = initDb();
+    if (err.type() != QSqlError::NoError) {
+        QMessageBox::critical(this, "Error initalizing database",
+                              "Error: " + dbError.text());
+        return;
+    }
+
 }
 
 mainWindow::~mainWindow()
