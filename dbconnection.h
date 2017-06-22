a880204fc2cb98e8d73666161f445c5ff5fd2f85
ermus19 <ermus19@hotmail.com>
2017-06-22 21:17:24 +0200

    Added queries to initDb()

--- dbconnection.h
+++ dbconnection.h
@@ -6,20 +6,25 @@
 #include <QSqlQuery>
 #include <QSqlError>
 
-
+#include <QStandardPaths>
+#include <QMessageBox>
+#include <QDir>
 
 QSqlError initDb()
 {
+    QSqlDatabase db;
+    QString dbName;
+
     //Datapath for apps data
     QString dataDir = QStandardPaths::writableLocation(QStandardPaths::DataLocation);
     QDir dir(dataDir);
     dir.mkpath(dir.path());
 
     //Datapath to the db file
-    dbName = dir.filePath(dbName+".sqlite");
+    dbName = dir.filePath("test.sqlite");
 
     //Instance of db using the datapath to the db   
-    db = QSqlDatabase::addDatabase("QSQLITE", dbName);
+    db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName(dbName);
 
     //Openning db and capturing error
@@ -29,15 +34,53 @@ QSqlError initDb()
     }
 
     //Initial queries
-    QsqlQuery query;
+    QSqlQuery query;
     if (!query.exec(QLatin1String("PRAGMA foreign_keys = ON")))
         return query.lastError();
 
-    if(!query.exec("CREATE TABLE IF NOT EXISTS account(account_id INTEGER PRIMARY KEY NOT NULL, name VARCHAR NOT NULL, balance REAL NULL, CONSTRAINT name_unique UNIQUE (name))"))
+    if(!query.exec("CREATE TABLE IF NOT EXISTS account("
+                   "account_id INTEGER PRIMARY KEY NOT NULL,"
+                   "name VARCHAR NOT NULL,"
+                   "balance REAL NOT NULL,"
+                   "creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,"
+                   "CONSTRAINT name_unique UNIQUE (name)"
+                   ")"))
         return query.lastError();
 
-    if(!query.exec(QLatin1String("")))
+    if(!query.exec("CREATE TABLE IF NOT EXISTS income("
+                   "income_id INTEGER PRIMARY KEY NOT NULL,"
+                   "account_id INTEGER NOT NULL,"
+                   "date DATETIME DEFAULT CURRENT_TIMESTAMP,"
+                   "periodic INTEGER NOT NULL,"
+                   "concept VARCHAR NOT NULL,"
+                   "description VARCHAR,"
+                   "FOREIGN KEY(account_id) REFERENCES account(account_id)"
+                   ")"))
         return query.lastError();
+
+    if(!query.exec("CREATE TABLE IF NOT EXISTS expense("
+                   "expense_id INTEGER PRIMARY KEY NOT NULL,"
+                   "account_id INTEGER NOT NULL,"
+                   "date DATETIME DEFAULT CURRENT_TIMESTAMP,"
+                   "concept VARCHAR NOT NULL,"
+                   "description VARCHAR,"
+                   "FOREIGN KEY(account_id) REFERENCES account(account_id)"
+                   ")"))
+        return query.lastError();
+
+    if(!query.exec("CREATE TABLE IF NOT EXISTS transfer("
+                   "transfer_id INTEGER PRIMARY KEY NOT NULL,"
+                   "origin_account_id INTEGER NOT NULL,"
+                   "destination_account_id INTEGER NOT NULL,"
+                   "date DATETIME DEFAULT CURRENT_TIMESTAMP,"
+                   "concept VARCHAR NOT NULL,"
+                   "description VARCHAR,"
+                   "FOREIGN KEY(origin_account_id) REFERENCES account(account_id),"
+                   "FOREIGN KEY(destination_account_id) REFERENCES account(account_id)"
+                   ")"))
+        return query.lastError();
+
+    return QSqlError();
 
 }
 
